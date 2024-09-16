16e04d237767579cae577ef1e0b1533cdea802ee
jordanxlau <jordanxlau@yahoo.ca>
2024-09-15 21:02:59 -0400

    implementation of Q2

--- lab1/_build/default/bin/main.ml
+++ lab1/_build/default/bin/main.ml
@@ -1,1 +1,16 @@
-let () = print_endline "Hello, World!"
+(* Question 1: map2 *)
+
+(* Question 2: filter_even *)
+
+(* This function from OCaml Documentation > Learn > Language > Lists *)
+let filter_even l = List.filter (fun x -> x mod 2 = 0) l ;;
+
+let string_of_int_list : int list -> string ;
+
+(* Question 3: compose_functions *)
+
+(* Question 4: reduce *)
+
+(* Example usage *)
+let () = print_endline "Question 2";;
+let () = print_endline (string_of_int_list (filter_even [8; 29; -10; 3; -1; 0; 4]));;
--- lab1/bin/main.ml
+++ lab1/bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+(* Question 1: map2 *)
+
+(* Question 2: filter_even *)
+
+(* This function from OCaml Documentation > Learn > Language > Lists *)
+let filter_even l = List.filter (fun x -> x mod 2 = 0) l ;;
+
+(* Question 3: compose_functions *)
+
+(* Question 4: reduce *)
