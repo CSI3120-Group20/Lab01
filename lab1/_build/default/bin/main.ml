702083c4d7c3280f0e82de908a350a367c7a7f3b
jordanxlau <jordanxlau@yahoo.ca>
2024-09-15 22:58:05 -0400

    bugfix

--- lab1/_build/default/bin/main.ml
+++ lab1/_build/default/bin/main.ml
@@ -5,12 +5,6 @@
 (* This function from OCaml Documentation > Learn > Language > Lists *)
 let filter_even l = List.filter (fun x -> x mod 2 = 0) l ;;
 
-let string_of_int_list : int list -> string ;
-
 (* Question 3: compose_functions *)
 
 (* Question 4: reduce *)
-
-(* Example usage *)
-let () = print_endline "Question 2";;
-let () = print_endline (string_of_int_list (filter_even [8; 29; -10; 3; -1; 0; 4]));;
--- lab1/bin/main.ml
+++ lab1/bin/main.ml
@@ -1,11 +1,11 @@
 (* Question 1: map2 *)
-let filter_even l = List.filter (fun x -> x mod 2 = 0) l ;;
 
 (* Question 2: filter_even *)
 
 (* This function from OCaml Documentation > Learn > Language > Lists *)
 let filter_even l = List.filter (fun x -> x mod 2 = 0) l ;;
 
+filter_even
 (* Question 3: compose_functions *)
 
 (* Question 4: reduce *)
