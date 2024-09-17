079780b2935aa1f036a85a72e76b4e7ce562b825
Henry <rtan035@uottawa.ca>
2024-09-16 20:22:35 -0400

    Implemented Q1 map2

--- lab1/bin/main.ml
+++ lab1/bin/main.ml
@@ -1,5 +1,11 @@
 (* Question 1: map2 *)
-
+let rec map2 f x y = 
+  if List.length x <> List.length y then raise (Failure "List length unequal!")
+  else 
+     match(x, y) with
+      | ([],[]) -> [] (*If both lists are empty, return empty list*)
+      | (x::xt,y::yt) -> (f x y) :: (map2 f xt yt);; (*Apply function to both lists*)
+      
 (* Question 2: filter_even *)
 let rec filter_even l = 
   match l with
