bd7ed0bbb180c29390fb5bc457ffa10be6a27e94
jordanxlau <jordanxlau@yahoo.ca>
2024-09-20 17:03:46 -0400

    added comments to main.ml

--- lab1/bin/main.ml
+++ lab1/bin/main.ml
@@ -7,14 +7,19 @@ let rec map2 f x y =
       | ([],_::_) -> []
       | (_::_,[]) -> []   
       | (x::xt,y::yt) -> (f x y) :: (map2 f xt yt);; (*Apply function to both lists*)
-        
+   
+(* call map2 *)
 map2
+
+
 (* Question 2: filter_even *)
 let rec filter_even l = 
-  match l with
-    | [] -> []
-    | h :: t -> (if h mod 2 = 0 then [h] else []) @ (filter_even t);;
+  (* `l`: list of ints to be filtered *)
+  match l with (*recursive function*)
+    | [] -> [] (*base case*)
+    | h :: t -> (if h mod 2 = 0 then [h] else []) @ (filter_even t);; (*only include the head character if it is even, then continue*)
 
+(* call filter_even *)
 filter_even
 
 
@@ -33,10 +38,15 @@ compose_functions
 
 
 (* Question 4: reduce *)
-
 let rec reduce f acc lst =
-  match lst with
-    | [] -> acc
-    | h :: t -> (reduce f (f acc h) t);;
+  (*  
+    `f`: function f
+    `acc`: initial value
+    `lst`: list to be reduced
+  *)
+  match lst with (*recursive function*)
+    | [] -> acc (*base case*)
+    | h :: t -> (reduce f (f acc h) t);; (*apply the function to acc and the head character, then continue*)
 
+(* call reduce *)
 reduce
