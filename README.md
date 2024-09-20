b310527cd125acda6668b16d3b058538e3dfef9d
jordanxlau <jordanxlau@yahoo.ca>
2024-09-20 09:19:07 -0400

    syntax errors fixed

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Lab 1
 
 To create executable:
-```$ opam exec -- dune build```
+```$ dune build```
 
 To launch the executable:
-```$ opam exec -- dune exec lab1```
+```$ dune exec lab1```
 
 To run in the OCaml repl (ensure you are in the ```Lab01\lab1\bin``` directory):
 ```opam exec -- ocaml```
 then: 
 ```
 #use "main.ml";;
-map2
+map2 (fun x y -> x + y) [1; 2; 3] [4; 5; 6];;
 filter_even [1; 2; 3; 4; 5; 6];;
 compose_functions
-reduce (fun x y -> x*y) 2 [1;2;3;4];; 
+reduce (fun x y -> x+y) 0 [1;2;3;4];; 
 ```
 
 Limited help (ie. understanding error messages, understanding certain operators) from ChatGPT may have been applied throughout the development of our solutions. Solutions have not been copy + pasted.
--- lab1/bin/main.ml
+++ lab1/bin/main.ml
@@ -1,26 +1,26 @@
 (* Question 1: map2 *)
 let rec map2 f x y = 
-        if List.length x <> List.length y then raise (Failure "List length unequal!")
-        else 
-           match(x, y) with
-            | ([],[]) -> [] (*If both lists are empty, return empty list*)
-            | ([],_::_) -> []
-            | (_::_,[]) -> []   
-            | (x::xt,y::yt) -> (f x y) :: (map2 f xt yt);; (*Apply function to both lists*)
-            
-map2(fun x y -> y / x)[1;3;4][6;5;7];     
+  if List.length x <> List.length y then raise (Failure "List length unequal!")
+  else 
+     match(x, y) with
+      | ([],[]) -> [] (*If both lists are empty, return empty list*)
+      | ([],_::_) -> []
+      | (_::_,[]) -> []   
+      | (x::xt,y::yt) -> (f x y) :: (map2 f xt yt);; (*Apply function to both lists*)
+        
+map2
 (* Question 2: filter_even *)
 let rec filter_even l = 
   match l with
-  | [] -> []
-  | h :: t -> (if h mod 2 = 0 then [h] else []) @ (filter_even t);;
+    | [] -> []
+    | h :: t -> (if h mod 2 = 0 then [h] else []) @ (filter_even t);;
 
 filter_even
 (* Question 3: compose_functions *)
 
 (* Question 4: reduce *)
 let rec reduce f acc lst = 
-  match lst with
+match lst with
   | [] -> acc
   | h :: t -> (f h (reduce f acc t));;
 
